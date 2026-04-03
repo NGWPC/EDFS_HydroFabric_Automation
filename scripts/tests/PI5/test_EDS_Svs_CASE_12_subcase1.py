b1e1cf1c4c820d701b6f8edd1d3bda488d85448c
Jyoti.Mikkilineni <Jyoti.P.Mikkilineni@rtx.com>
2026-04-03 17:59:40 +0000

    Adding PI5 subcase 1 details

---
+++ scripts/tests/PI5/test_EDS_Svs_CASE_12_subcase1.py
@@ -0,0 +1,13 @@
+import pytest
+from ..utils import compare_images_util
+
+def test_EDS_Svs_CASE_12_subcase1(test_name,load_scenario_data,scenarios,fetch_docker_details,run_docker_script,fetch_notebook_location_details):
+    tn = test_name + '.json'
+    folder_name = 'PI5/data'
+
+    docker_commands,dir_paths = fetch_docker_details(folder_name,tn,scenarios)
+    for docker_command in docker_commands:
+         run_docker_script(folder_name,tn,scenarios,docker_command)
+         print("The command ran successfully.")
+    input_notebook_location, output_notebook_location = fetch_notebook_location_details(folder_name,tn,scenarios)
+    compare_images_util.verify_gdf_output(os.path.expanduser(output_notebook_location),tag_name = 'compare_image')
